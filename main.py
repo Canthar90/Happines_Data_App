5a731bc85b1138be573fdf74cc6d749cc9c8df96
Canthar90 <d.cieslak90@gmail.com>
2023-01-09 13:48:23 +0100

    Added side description

--- main.py
+++ main.py
@@ -13,6 +13,17 @@ st.set_page_config(
 
 st.title("In search for Happiness")
 
+cols = df.columns
+message ="This app allow you to create scatter plot from data about happiness\
+    you can choose values for x and y axis. Available values"
+message_add = [(column + ', ') for column in cols]
+mess2 = ""
+for mess in message_add:
+    mess2 += mess
+message =  message + mess2
+
+st.info(message)
+
 x_option = st.selectbox("Select data for X axis", (df.columns))
 
 y_option = st.selectbox("Select data for Y axis", (df.columns))
@@ -24,3 +35,6 @@ y = df[y_option]
 plot = px.scatter(x=x, y=y, labels={"x": x_option, "y": y_option})
 
 st.plotly_chart(plot)
+
+
+st.info("Created by Dawid Cieślak")
